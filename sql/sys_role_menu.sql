e32b8276266ae530072cfac3d8113b90019b55e6
zay <zhouanyuxx@outlook.com>
2023-09-01 17:46:45 +0800

    sql数据同步

--- sql/sys_role_menu.sql
+++ sql/sys_role_menu.sql
@@ -1,7 +1,9 @@
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (1, 1);
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (4, 23);
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (4, 24);
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (4, 25);
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (7, 16);
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (7, 18);
-INSERT INTO dormitorymanagementsystem.sys_role_menu (role_id, menu_id) VALUES (7, 19);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (1, 1);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (3, 102);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (3, 103);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (4, 23);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (4, 24);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (4, 25);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (7, 16);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (7, 18);
+INSERT INTO yu.sys_role_menu (role_id, menu_id) VALUES (7, 19);
